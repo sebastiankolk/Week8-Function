37c50c33102ded8958a549f57a10687e7e9fdc5b
Opilane TTHK <opilane@tthk.ee>
2021-10-20 09:29:26 +0300

    functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta esimene number ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("sisesta teine number");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("vali tehe ('+', '-', '*' või '/'");
+            char useroperation = Convert.ToChar(Console.ReadLine());
+
+            switch (useroperation)
+            {
+                case '+':
+                    AddTwoNumbers(number1, number2);
+                    break;
+                case '-':
+                    SubstractTwoNumbers(number1, number2);
+                    break;
+                case '*':
+                    KorrutaTwoNumbers(number1, number2);
+                    break;
+                case '/':
+                    DivideTwoNumbers(number1, number2);
+                    break;
+                default:
+                    Console.WriteLine("invalid tehe");
+                    break;
+            }
+
+        }
+
+        private static void AddTwoNumbers(int x, int y)
+        {
+            int result = x + y;
+            Console.WriteLine($"{x} + {y} = {result}");
+        }
+
+        private static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine($"{x} / {y} = {result}");
+        }
+        private static void SubstractTwoNumbers(double x, double y)
+        {
+            double result = x - y;
+            Console.WriteLine($"{x} - {y} = {result}");
+        }
+        private static void KorrutaTwoNumbers(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine($"{x} * {y} = {result}");
+
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("sisesta oma eesnimi.");
+            string username = Console.ReadLine();
+            Console.WriteLine("sisesta oma perekonna nimi.");
+            string username2 = Console.ReadLine();
+            Console.WriteLine("sisesta oma vanus");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+            Greetings(username, username2, userAge);
+        }
+
+        private static void Greetings(string someName,string SomeName2, int someNumber)
+        {
+            Console.WriteLine($"Tere, {someName} {SomeName2}");
+            Console.WriteLine($"oled sündinud aastal {2021 - someNumber}.");
+
+        }
+    }
+}
